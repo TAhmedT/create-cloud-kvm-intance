a06607aefc2752a93dad4b6a7b8c409071018f98
Akhmed Taaev <ahmedtaaev@gmail.com>
2024-03-10 21:40:11 +0300

    Refactoring main script, add setup script

--- create-vm.sh
+++ create-vm.sh
@@ -2,88 +2,175 @@
 
 set -e
 
-die() { echo "ERR: $@" >&2 ; exit 2 ; }
-
-function prechecks() {
-    test -f $ISO_PATH/centos8.qcow2 || die "Check centos8 cloud image exists"
-    test -f $ISO_PATH/ubuntu2204.qcow2 || die "Check ubuntu2204 cloud image exists"
-
-    if test $DISTR = "centos"; then
-        IMAGE_NAME="centos8.qcow2"
-        CLOUD_INIT_CONFIG_PATH="$SCRIPT_DIR/centos-config"
-        OS_VARIANT="centos8"
-    elif test $DISTR = "ubuntu"; then
-        IMAGE_NAME="ubuntu2204.qcow2"
-        CLOUD_INIT_CONFIG_PATH="$SCRIPT_DIR/ubuntu-config"
-        OS_VARIANT="ubuntu22.04"
-    else
-        die "Distr not correct"
-    fi
-
-}
-
-function cleanup_vm() {
-
-    virsh destroy $VM_NAME || true
-    virsh undefine $VM_NAME || true
-    rm -rf $LIBVIRT_IMAGES_PATH
-    lvremove cloud00/$VM_NAME || true
-}
-
-function create_disk() {
-
-    lvcreate -n cloud00/$VM_NAME -L $DISK_SIZE
-    qemu-img convert $ISO_PATH/$IMAGE_NAME -O raw /dev/mapper/cloud00-${VOLUME_NAME}
-}
-
-
-VM_NAME=${1:-test}
-DISK_SIZE=${2:-40G}
-DISTR=${3:-ubuntu}
-VCPUS=${4:-2}
-RAM=${5:-2048M}
+VM_NAME="test"
+VCPUS_COUNT="2"
+MEMORY_AMOUNT="2048"
+DISK_SIZE="40G"
+LINUX_DISTRIBUITION=""
+IP_ADDRESS=""
+SUBNET=""
+DEFAULT_GATEWAY=""
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 ISO_PATH="${SCRIPT_DIR}/ISO"
-LIBVIRT_IMAGES_PATH=/var/lib/libvirt/images/$VM_NAME
-VOLUME_NAME="${VM_NAME//-/--}"
+LIBVIRT_IMAGES_PATH=""
+VOLUME_NAME=""
 CLOUD_INIT_CONFIG_PATH=""
 IMAGE_NAME=""
 OS_VARIANT=""
 
-echo "Prechecks before setup"
-prechecks
 
-echo "Cleanup old VM"
-cleanup_vm
+function usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  --name <VM_NAME>                Specify VM name. Default value 'test'"
+    echo "  --vcpus <VCPUS_COUNT>           Specify CPU counts. Default value 2"
+    echo "  --memory <MEMORY_AMOUNT>        Specify memory amount in MB. Default value 2048"
+    echo "  --disk <DISK_SIZE>              Specify disk size. Default value 40G"
+    echo "  --distr <LINUX_DISTRIBUITION>   Specify distribution(centos/ubuntu)"
+    echo "  --ip <IP_ADDRESS>               Specify static ip"
+    echo "  --subnet <SUBNET>               Specify subnet. Example --subnet 24 or 32"
+    echo "  --gateway <DEFAULT_GATEWAY>     Specify default gateway"
+    echo "  -h, --help                      Show this help message"
+}
 
-echo "Create disk"
-create_disk
+function prechecks_and_fill_vars() {
+    LIBVIRT_IMAGES_PATH=/var/lib/libvirt/images/$VM_NAME
+    VOLUME_NAME="${VM_NAME//-/--}"
 
-echo "Generate cloud-init config"
-cat  > $CLOUD_INIT_CONFIG_PATH/meta-data << EOF
-#cloud-config
-instance-id: $VM_NAME
-local-hostname: $VM_NAME
+    if test $LINUX_DISTRIBUITION = "centos"; then
+        IMAGE_NAME="centos8.qcow2"
+        CLOUD_INIT_CONFIG_PATH="$SCRIPT_DIR/centos-config"
+        OS_VARIANT="centos8"
+    elif test $LINUX_DISTRIBUITION = "ubuntu"; then
+        IMAGE_NAME="ubuntu2204.qcow2"
+        CLOUD_INIT_CONFIG_PATH="$SCRIPT_DIR/ubuntu-config"
+        OS_VARIANT="ubuntu22.04"
+    else
+        echo "distribution not correct"
+        exit 2
+    fi
+
+    if [ -z $IP_ADDRESS ]; then
+        echo "Specify the ip address"
+        exit 2
+    fi
+
+    if [ -z $SUBNET ]; then
+        echo "Specify the subnet address"
+        exit 2
+    fi
+}
+
+function cleanup_old_vm() {
+
+    virsh destroy $VM_NAME || true
+    virsh undefine $VM_NAME || true
+    rm -rf $LIBVIRT_IMAGES_PATH
+    lvremove cloud00/$VM_NAME -y || true
+}
+
+function create_vm_disk() {
+
+    lvcreate -n cloud00/$VM_NAME -L $DISK_SIZE -y
+    qemu-img convert $ISO_PATH/$IMAGE_NAME -O raw /dev/mapper/cloud00-${VOLUME_NAME}
+}
+
+function generate_cloud_init_config() {
+
+
+    #GENERATE meta-data
+    cat > $CLOUD_INIT_CONFIG_PATH/meta-data << EOF
+    #cloud-config
+    instance-id: $VM_NAME
+    local-hostname: $VM_NAME
 EOF
 
-genisoimage -output $SCRIPT_DIR/config.iso -V cidata -r -J $CLOUD_INIT_CONFIG_PATH/user-data \
-$CLOUD_INIT_CONFIG_PATH/meta-data \
-$CLOUD_INIT_CONFIG_PATH/network-config
 
-echo "Copy cloud-init config to libvirt dir"
-mkdir $LIBVIRT_IMAGES_PATH
-cp $SCRIPT_DIR/config.iso $LIBVIRT_IMAGES_PATH/
-rm $SCRIPT_DIR/config.iso
+    #GENERATE network-config
+    if test $LINUX_DISTRIBUITION = "centos"; then
+        cat > ${CLOUD_INIT_CONFIG_PATH}/network-config << EOF
+#cloud-config
+network:
+  version: 1
+  config:
+    - type: physical
+      name: eth0
+      subnets:
+        - type: static
+          address: ${IP_ADDRESS}/${SUBNET}
+          gateway: ${DEFAULT_GATEWAY}
+          dns_nameservers:
+            - 8.8.8.8
+            - 8.8.4.4
+EOF
+
+    else
+        cat > ${CLOUD_INIT_CONFIG_PATH}/network-config << EOF
+#cloud-config
+network:
+  version: 2
+  ethernets:
+    enp1s0:
+      addresses:
+        - ${IP_ADDRESS}/${SUBNET}
+      gateway4: ${DEFAULT_GATEWAY}
+      nameservers:
+        addresses:
+            - 8.8.8.8
+            - 8.8.4.4
+EOF
+    fi    
+
+
+    genisoimage -output $SCRIPT_DIR/config.iso -V cidata -r -J $CLOUD_INIT_CONFIG_PATH/user-data \
+    $CLOUD_INIT_CONFIG_PATH/meta-data \
+    $CLOUD_INIT_CONFIG_PATH/network-config
+
+    echo "Copy cloud-init config to libvirt dir"
+    mkdir $LIBVIRT_IMAGES_PATH
+    cp ${SCRIPT_DIR}/config.iso ${LIBVIRT_IMAGES_PATH}/
+    rm ${SCRIPT_DIR}/config.iso
+}
+
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --name) VM_NAME="$2"; shift;;
+        --vcpus) VCPUS_COUNT="$2"; shift ;;
+        --memory) MEMORY_AMOUNT="$2"; shift ;;
+        --disk) DISK_SIZE="$2"; shift ;;
+        --distr) LINUX_DISTRIBUITION="$2"; shift ;;
+        --ip) IP_ADDRESS="$2"; shift ;;
+        --subnet) SUBNET="$2"; shift ;;
+        --gateway) DEFAULT_GATEWAY="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        *) echo "Unknown option $1"; usage; exit 1 ;;
+    esac
+    shift
+done
+
+echo "Prechecks and filling in variables"
+prechecks_and_fill_vars
+
+echo "Cleanup old VM"
+cleanup_old_vm
+
+
+echo "Create VM disk"
+create_vm_disk
+
+echo "Generate cloud-init config"
+generate_cloud_init_config
 
 
 echo "Create VM"
-virt-install --ram ${RAM} \
---vcpus ${VCPUS} \
+virt-install --ram ${MEMORY_AMOUNT} \
+--vcpus ${VCPUS_COUNT} \
 --name ${VM_NAME} \
 --disk path=/dev/mapper/cloud00-${VOLUME_NAME} \
 --disk path=${LIBVIRT_IMAGES_PATH}/config.iso,device=cdrom \
 --os-variant ${OS_VARIANT} \
---network network=virbr0,model=virtio \
+--network network=default,model=virtio \
 --graphics none \
 --noautoconsole \
 --import
